664ef1bfd79cfb6ce3f1a09887375551b7183558
lordjimen <emiliyan.pavlov@sentient-autonomics.com>
2023-04-17 19:28:29 +0300

    Remove redundant assembly `using`-s.

--- src/DataStructures/SuccinctDataStructures/BitIndices/Bits.cs
+++ src/DataStructures/SuccinctDataStructures/BitIndices/Bits.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using System.Diagnostics;
-using System.Collections.Generic;
+﻿using System.Diagnostics;
 using System.Collections.Immutable;
 using System.Runtime.CompilerServices;
 
